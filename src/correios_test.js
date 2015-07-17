7bbc00d978c3b0a19332995fc07eb7ddb6687d39
Victor Queiroz <victorcqueirozg@gmail.com>
2015-07-17 08:53:29 -0300

    tests, dist folder and gulp build task implemented

--- src/correios_test.js
+++ src/correios_test.js
@@ -2,24 +2,30 @@ describe('angular-correios', function () {
 	var Correios, $httpBackend;
 	beforeEach(module('angular-correios'));
 	beforeEach(inject(function (_Correios_, _$httpBackend_) {
-		Correios = _Correios_
-		$httpBackend = _$httpBackend_
-	}))
+		Correios = _Correios_;
+		$httpBackend = _$httpBackend_;
+	}));
+
 	afterEach(function() {
 		$httpBackend.verifyNoOutstandingExpectation();
 		$httpBackend.verifyNoOutstandingRequest();
 	});
+
 	it('should get a postal code', function () {
-		$httpBackend.expectGET('http://cep.correiocontrol.com.br/02011200.json').respond({
-			bairro: "Santana",
+		$httpBackend.expectGET('//viacep.com.br/ws/02011200/json').respond({
+			cep: "02011-200",
 			logradouro: "Rua Voluntários da Pátria",
-			cep: "02011200",
+			complemento: "de 1413 a 1541 - lado ímpar",
+			bairro: "Santana",
+			localidade: "São Paulo",
 			uf: "SP",
-			localidade: "São Paulo"
+			ibge: "3550308"
 		});
 
 		Correios.get('02011200').then(function (postalCode) {
-			assert.equal('02011200', postalCode.cep);
+			assert.equal('02011-200', postalCode.cep);
+			assert.equal('SP', postalCode.uf);
+			assert.equal('3550308', postalCode.ibge);
 		});
 
 		$httpBackend.flush();
